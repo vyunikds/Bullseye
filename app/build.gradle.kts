eb7886387189f042cbf1286682c0204cfcb9058b
Dmitriy Vyunik <vyunik-ds@zvuk.com>
2025-04-19 13:49:18 +0300

    removed older version of splashscreen lib in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.splashscreen)
-//    implementation ("androidx.core:core-splashscreen:1.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
